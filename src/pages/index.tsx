a4bac7a87d2ba384951b86280edf99208838afeb
Hamza-ARROUBAT <ht.arroubat@gmail.com>
2022-04-24 10:54:42 +0000

    added TextBubble ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,44 @@
 import type { NextPage } from 'next';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
-
-const Editor = dynamic(() => import('../components/Editor'), { ssr: false });
+import { useState } from 'react';
+import styled from 'styled-components';
+import TextBubble from '../components/TextBubble';
 
 const Home: NextPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const Editor = dynamic(
+    async () => {
+      const imported = await import('../components/Editor');
+      setIsLoading(false);
+      return imported;
+    },
+    {
+      ssr: false,
+    }
+  );
   return (
-    <div>
+    <Container>
       <Head>
         <title>LumCode</title>
         <meta name="description" content="LumCode" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      {!isLoading && (
+        <TextBubble text={'Try to display the word "1" in the console !'} />
+      )}
       <Editor />
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+  padding: 0 5em;
+`;
 
 export default Home;
