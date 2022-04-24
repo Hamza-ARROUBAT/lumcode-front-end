c80b9b9d2eeb21cd25d58aeda59761991815965a
Hamza-ARROUBAT <ht.arroubat@gmail.com>
2022-04-24 09:26:24 +0000

    rename genericPage 2

--- src/pages/generic-page/genericPage.tsx
+++ src/pages/generic-page/genericPage.tsx
@@ -1,4 +1,4 @@
-import { GenericPageStyle } from './GenericPage.style';
+import { GenericPageStyle } from './genericPage.style';
 
 export default function GenericPage() {
   return <GenericPageStyle.Container>GenericPage</GenericPageStyle.Container>;
