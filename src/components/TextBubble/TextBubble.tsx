8f5003aceae1c452d669e5d97779c3a202c5a204
Hamza-ARROUBAT <ht.arroubat@gmail.com>
2022-04-30 09:21:10 +0000

    added Recoil + check-correct + fixed rerender

--- src/components/TextBubble/TextBubble.tsx
+++ src/components/TextBubble/TextBubble.tsx
@@ -5,36 +5,32 @@ import { Dispatch, SetStateAction } from 'react';
 interface ITextBubble {
   background: string;
   text: string;
-  isTextSkipped: boolean;
-  setIsTextSkipped: Dispatch<SetStateAction<boolean>>;
+  // isTextSkipped: boolean;
+  // setIsTextSkipped: Dispatch<SetStateAction<boolean>>;
 }
 
-export default function TextBubble({
-  background,
-  text,
-  isTextSkipped,
-  setIsTextSkipped,
-}: ITextBubble) {
+export default function TextBubble(props: ITextBubble) {
   return (
     <TextBubbleStyle.Container
-      background={background}
-      onClick={() => setIsTextSkipped(true)}
+      background={props.background}
+      // onClick={() => props.setIsTextSkipped(true)}
     >
-      {isTextSkipped ? (
-        <>{text}</>
-      ) : (
-        <AnimatedText
-          type="chars"
-          interval={0.05}
-          duration={0.1}
-          animation={{
-            y: '-5px',
-            ease: 'ease',
-          }}
-        >
-          {text}
-        </AnimatedText>
-      )}
+      {/* {props.isTextSkipped ? (
+        <>{props.text}</>
+      ) : ( */}
+      {/* <AnimatedText
+        type="chars"
+        interval={0.05}
+        duration={0.1}
+        animation={{
+          y: '-5px',
+          ease: 'ease',
+        }}
+      >
+        {props.text}
+      </AnimatedText> */}
+      {/* )} */}
+      <p>lol</p>
     </TextBubbleStyle.Container>
   );
 }
